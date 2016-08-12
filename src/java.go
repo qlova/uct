2fb998be4ded77657a14e14f1cf69f4fbad24d8f
Quentin Quaadgras <quentin@splizard.com>
2016-08-12 23:01:56 +1200

    Go fmt java.go

--- src/java.go
+++ src/java.go
@@ -3,22 +3,23 @@ package main
 import "flag"
 
 var JavaReserved = []string{
-	"abstract","continue","for","new","switch","assert","default",
-	"goto","package","synchronized","boolean","do","if","private",
-	"this","break","double","implements","protected","throw",
-	"byte","else","import","public","throws","case","enum","instanceof",
-	"return","transient","catch","extends","int","short","try",
-	"char","final","interface","static","void","class","finally",
-	"long","strictfp","volatile","const","float","native","super","while",
+	"abstract", "continue", "for", "new", "switch", "assert", "default",
+	"goto", "package", "synchronized", "boolean", "do", "if", "private",
+	"this", "break", "double", "implements", "protected", "throw",
+	"byte", "else", "import", "public", "throws", "case", "enum", "instanceof",
+	"return", "transient", "catch", "extends", "int", "short", "try",
+	"char", "final", "interface", "static", "void", "class", "finally",
+	"long", "strictfp", "volatile", "const", "float", "native", "super", "while",
 }
 
 //This is the Java compiler for uct.
 var Java bool
+
 func init() {
 	flag.BoolVar(&Java, "java", false, "Target Java")
-	
+
 	RegisterAssembler(JavaAssembly, &Java, "java", "//")
-	
+
 	for _, word := range JavaReserved {
 		JavaAssembly[word] = Reserved()
 	}
@@ -27,102 +28,101 @@ func init() {
 var JavaAssembly = Assemblable{
 	//Special commands.
 	"HEADER": Instruction{
-		Data: "public class %s {",
+		Data:   "public class %s {",
 		Indent: 1,
-		Args: 1,
+		Args:   1,
 	},
-	
+
 	"FOOTER": Instruction{
-		Data: "}",
-		Indent: -1,
+		Data:        "}",
+		Indent:      -1,
 		Indentation: -1,
 	},
-	
+
 	"FILE": Instruction{
 		Data: JavaFile,
 		Path: "/Stack.java",
 	},
-	
+
 	"NUMBER": is("new Stack.Number(%s)", 1),
-	"SIZE": is("%s.size()", 1),
+	"SIZE":   is("%s.size()", 1),
 	"STRING": is("new Stack.Array(%s)", 1),
-	"ERROR": is("stack.ERROR", 1),
+	"ERROR":  is("stack.ERROR", 1),
 
 	"SOFTWARE": Instruction{
-		Data: "public static void main(String[] args) { Stack stack = new Stack(); stack.Arguments = args;",
+		Data:   "public static void main(String[] args) { Stack stack = new Stack(); stack.Arguments = args;",
 		Indent: 1,
 	},
 	"EXIT": Instruction{
-		Indented: 2,
-			Data: "}\n",
-			Indent: -1,
-			Indentation: -1,
+		Indented:    2,
+		Data:        "}\n",
+		Indent:      -1,
+		Indentation: -1,
 		Else: &Instruction{
 			Data: "System.exit(stack.ERROR);",
 		},
 	},
-	
+
 	"FUNCTION": is("static void %s(Stack stack) {", 1, 1),
-	"RETURN": is("}",0,-1, -1),
-	
-	
+	"RETURN":   is("}", 0, -1, -1),
+
 	"PUSH": is("stack.push(%s);", 1),
 	"PULL": is("Stack.Number %s = stack.pull();", 1),
-	
-	"PUT": is("stack.put(%s);", 1),
-	"POP": is("Number %s = stack.pop();", 1),
+
+	"PUT":   is("stack.put(%s);", 1),
+	"POP":   is("Number %s = stack.pop();", 1),
 	"PLACE": is("stack.place(%s);", 1),
 
-	"ARRAY": is("Stack.Array %s = stack.array();", 1),
+	"ARRAY":  is("Stack.Array %s = stack.array();", 1),
 	"RENAME": is("%s = stack.ActiveArray", 1),
-	
+
 	"SHARE": is("stack.share(%s);", 1),
-	"GRAB": is("ARRAY %s = stack.grab();", 1),
-	
+	"GRAB":  is("ARRAY %s = stack.grab();", 1),
+
 	"RELAY": is("stack.relay(%s);", 1),
-	"TAKE": is("PIPE %s = stack.take();", 1),
-	
+	"TAKE":  is("PIPE %s = stack.take();", 1),
+
 	"GET": is("Stack.Number %s = stack.get();", 1),
 	"SET": is("stack.set(%s);", 1),
-	
+
 	"VAR": is("Stack.Number %s = new Number();", 1),
-	
-	"OPEN": is("stack.open();"),
-	"LOAD": is("stack.load();"),
-	"OUT": is("stack.out();"),
-	"IN": is("stack.in();"),
+
+	"OPEN":   is("stack.open();"),
+	"LOAD":   is("stack.load();"),
+	"OUT":    is("stack.out();"),
+	"IN":     is("stack.in();"),
 	"STDOUT": is("stack.stdout();"),
-	"STDIN": is("stack.stdin();"),
-	
+	"STDIN":  is("stack.stdin();"),
+
 	"CLOSE": is("%s.close();", 1),
-	
-	"LOOP": is("while (true) {"),
-	"BREAK": is("break"),
+
+	"LOOP":   is("while (true) {"),
+	"BREAK":  is("break"),
 	"REPEAT": is("}"),
-	
-	"IF": is("if (%s.compareTo(new Number(1)) == 0 ) {", 1, 1),
+
+	"IF":   is("if (%s.compareTo(new Number(1)) == 0 ) {", 1, 1),
 	"ELSE": is("} else {", 0, 0, -1),
-	"END": is("}", 0, -1, -1),
-	
-	"RUN": is("%s();", 1),
+	"END":  is("}", 0, -1, -1),
+
+	"RUN":  is("%s();", 1),
 	"DATA": is("static Stack.Array %s = %s;", 2),
-	
+
 	"FORK": is("{ Stack s = stack.copy(); pool__java.execute(() -> %s(s)); }\n"),
-	
+
 	"ADD": is("%s = %s.add(%s);"),
 	"SUB": is("%s = %s.sub(%s);"),
 	"MUL": is("%s = %s.mul(%s);"),
 	"DIV": is("%s = %s.div(%s);"),
 	"MOD": is("%s = %s.mod(%s);"),
 	"POW": is("%s = %s.pow(%s);"),
-	
-	"SLT": is("%s = %s.slt(%s);"),
-	"SEQ": is("%s = %s.seq(%s);"),
-	"SGE": is("%s = %s.sge(%s);"),
-	"SGT": is("%s = %s.sgt(%s);"),
-	"SNE": is("%s = %s.sne(%s);"),
-	"SLE": is("%s = %s.sle(%s);"),
-	
+
+	"SLT": is("%s = %s.compareTo(%s) == -1 ? Stack.One : Stack.Zero;"),
+	"SEQ": is("%s = %s.compareTo(%s) == 0 ? Stack.One : Stack.Zero;"),
+	"SGE": is("%s = %s.compareTo(%s) >= 0 ? Stack.One : Stack.Zero;"),
+	"SGT": is("%s = %s.compareTo(%s) == 1 ? Stack.One : Stack.Zero;"),
+	"SNE": is("%s = %s.compareTo(%s) != 0 ? Stack.One : Stack.Zero;"),
+	"SLE": is("%s = %s.compareTo(%s) <= 0 ? Stack.One : Stack.Zero;"),
+
 	"JOIN": is("%s = %s.join(%s);"),
 }
 
@@ -170,6 +170,8 @@ public class Stack {
     //This will store the system arguments.
     public static String[] Arguments;
 
+  	static final Number Zero = (Number)Number.ZERO;
+    static final Number One = (Number)Number.ONE;
 
     //This is the threading pool.
     public static Executor ThreadPool = Executors.newCachedThreadPool();
