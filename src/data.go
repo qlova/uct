7c7ab8f4beda21a98ebc22006e6b677cea5c8dc7
Splizard <splizard@splizard.com>
2017-09-14 10:17:08 +1200

    Fix python HeapIT handling.

--- src/data.go
+++ src/data.go
@@ -170,7 +170,7 @@ func dataStackPy() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "data/stack.py", size: 9090, mode: os.FileMode(436), modTime: time.Unix(1503917565, 0)}
+	info := bindataFileInfo{name: "data/stack.py", size: 9090, mode: os.FileMode(436), modTime: time.Unix(1505339279, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
